b620096e706e1f562d3222f01ad1bf31d8de3a2e
Suman Patel <67832813+suman-patel0@users.noreply.github.com>
2021-05-23 19:09:40 +0530

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,26 +1,29 @@
 s = input()
-occurence = {i : s.count(i) for i in s}
-min_occ = min(list(occurence.values()))
-extra = 0
-mystring = True
+
+occurence = {i : s.count(i) for i in s}       ##this dictionary has count of all the distinct characters in input string
+
+min_occ = min(list(occurence.values()))       ##the minimum count
+
+extra = 0                                     ##variable to check if any character has more than one extra count
+mystring = True                               ##variable to check if the given input is my string or not
+
 for i in list(occurence.values()):
-    if i - min_occ == 1:
+    
+    if i - min_occ == 1:                      ##if a character has one extra count than the min count then increase the extra variable
         extra = extra + 1
-        if extra > 1:
+        
+        if extra > 1:                         ##if more than one character have extra counts then  it is not my string and come out of the loop
            mystring = False
            break
-    elif i-min_occ == 0:
+            
+    elif i-min_occ == 0:                      ##if a character has same count as that of the min count then no issue
         continue
-    else:
+        
+    else:                                     ##if a character has more than one count then it is not my string and come out of the loop
         mystring = False
         break
+        
 if mystring == True:
     print('MY STRING')
 else:
     print('NOT MY STRING')
-    
-    
-              
-        
-
-              
